9cb95e00af4019b6405efa093c6dd4fc67a74805
Sanae Kochiya <lvl5weeaboo@gmail.com>
2018-08-20 18:44:54 -0600

    make it public or suffer the consequences

--- src/main/java/com/yamada/notkayla/Kayla.java
+++ src/main/java/com/yamada/notkayla/Kayla.java
@@ -17,7 +17,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Kayla {
-    static Logger log = Logger.getLogger("Kayla");
+    public static Logger log = Logger.getLogger("Kayla");
     static JDA bot;
     public static CommandRegistry registry = new CommandRegistry();
     public static void main(String[] args){
--- src/main/java/com/yamada/notkayla/database/Database.java
+++ src/main/java/com/yamada/notkayla/database/Database.java
@@ -1,4 +1,16 @@
 package com.yamada.notkayla.database;
 
+import com.yamada.notkayla.Kayla;
+
+import java.util.logging.Level;
+
 public class Database {
+    public static void init(){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            Kayla.log.log(Level.SEVERE,"Uh oh, looks like the driver wasn't found for the database ;P");
+            e.printStackTrace();
+        }
+    }
 }
