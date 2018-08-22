ff790415782eb56677bf9c5bfb43d98273228981
tangalbert919 <tangalbert919@gmail.com>
2018-08-22 08:43:51 -0500

    In case mods have a reason, send the banned player a reason

--- src/main/java/com/yamada/notkayla/commands/mod/BanCommand.java
+++ src/main/java/com/yamada/notkayla/commands/mod/BanCommand.java
@@ -8,10 +8,19 @@ public class BanCommand implements Command {
 
     @Override
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
-
+        StringBuilder reason = new StringBuilder();
         try {
-            event.getGuild().getController().ban(event.getGuild().getMember(event.getMessage().getMentionedUsers().get(0))).queue();
-        }catch (Exception e) {
+            if (args.length == 2) {
+                event.getGuild().getController().ban(event.getGuild().getMember(event.getMessage().getMentionedUsers().get(0)), 0).queue();
+            }
+            else {
+                for (int i = 2; i < args.length; i++) {
+                    reason.append(args[i]).append(" ");
+                }
+                bot.getPrivateChannelById(event.getMember().getUser().getId()).sendMessage("You have been banned for this reason: " + reason).queue();
+                event.getGuild().getController().ban(event.getGuild().getMember(event.getMessage().getMentionedUsers().get(0)), 0, String.valueOf(reason)).queue();
+            }
+        } catch (Exception e) {
             event.getChannel().sendMessage("I couldn't ban the specified user.").queue();
             return;
         }
