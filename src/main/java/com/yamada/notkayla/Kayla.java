5ceeb94e2d656dd429012dfc5e3310b226a66e87
tangalbert919 <tangalbert919@gmail.com>
2018-08-22 08:32:14 -0500

    Add and register report command

--- src/main/java/com/yamada/notkayla/Kayla.java
+++ src/main/java/com/yamada/notkayla/Kayla.java
@@ -62,6 +62,7 @@ public class Kayla {
         registry.register("ping", new PingCommand());
         registry.register("pull", new PullCommand());
         registry.register("user", new UserCommand());
+        registry.register("report", new ReportCommand());
         registry.register("eval", new EvalCommand());
         registry.register("dog", new RandomDogCommand());
         registry.register("cat", new RandomCatCommand());
