3f909eb74b1731967c77114c46b83c39027493bf
Sanae6 <atmatm6000@outlook.com>
2018-08-24 16:37:03 -0600

    ..

--- src/main/java/com/yamada/notkayla/commands/Command.java
+++ src/main/java/com/yamada/notkayla/commands/Command.java
@@ -9,7 +9,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
+//@Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
     String name();
     String description() default "Not yet set.";
