c866422f7afa68523972a11b701313ba7e5e3178
Sanae6 <atmatm6000@outlook.com>
2018-08-24 16:26:34 -0600

    dying internale

--- src/main/java/com/yamada/notkayla/commands/Command.java
+++ src/main/java/com/yamada/notkayla/commands/Command.java
@@ -3,6 +3,13 @@ package com.yamada.notkayla.commands;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
     String name();
     String description() default "Not yet set.";
