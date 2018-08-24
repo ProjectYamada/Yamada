ae05517aabbcd6122f3e3f7b68157f9be4c08113
Sanae6 <atmatm6000@outlook.com>
2018-08-24 14:45:30 -0600

    ... ok

--- src/main/java/com/yamada/notkayla/module/Adapter.java
+++ src/main/java/com/yamada/notkayla/module/Adapter.java
@@ -2,7 +2,6 @@ package com.yamada.notkayla.module;
 
 import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -18,7 +17,7 @@ public abstract class Adapter {
         attachedClass = clazz;
         ReflectionUtils.getMethods(clazz);
     }
-    public Object runMethod(String method,String[] args) throws NotImplementedException {
+    public Object runMethod(String method,String[] args) {
         return null;
     }
 }
