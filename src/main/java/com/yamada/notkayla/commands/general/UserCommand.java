cda48187bbbb097589828585c627b8a78a690fe8
Albert Tang <tangalbert919@gmail.com>
2018-08-28 17:00:19 -0500

    Improve user command, start on hug command

--- src/main/java/com/yamada/notkayla/commands/general/UserCommand.java
+++ src/main/java/com/yamada/notkayla/commands/general/UserCommand.java
@@ -3,24 +3,39 @@ package com.yamada.notkayla.commands.general;
 import com.yamada.notkayla.commands.Command;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 import java.awt.*;
+import java.util.List;
 
 @Command(name="user",group="general",description="View information on a server member")
 public class UserCommand {
     public UserCommand(){}
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
-        //TODO: Allow people to get user information from other people.
-        //String arg = event.getMessage().getContentRaw().substring("!yuser ".length());
+        //TODO: Test this code
+        User user;
+        Member member;
+        try {
+            List<User> mentionedUsers = event.getMessage().getMentionedUsers();
+            List<Member> mentionedMembers = event.getMessage().getMentionedMembers();
+            if (mentionedUsers.size() > 1) event.getChannel().sendMessage("You specified more than one user. We will only get information about the first mentioned user.").queue();
+            user = mentionedUsers.get(0);
+            member = mentionedMembers.get(0);
+        } catch (Exception e) {
+            user = event.getAuthor();
+            member = event.getMember();
+        }
         EmbedBuilder embed = new EmbedBuilder();
         embed.setTitle("Information collected!");
         embed.setDescription("Here's what we know about this user!");
         embed.setColor(new Color(0xe91e63));
         embed.setFooter(String.format("Hello, %s", event.getAuthor().getName()), event.getAuthor().getAvatarUrl());
         embed.setThumbnail(event.getAuthor().getAvatarUrl());
-        embed.addField("User ID: ", event.getAuthor().getId(), false);
-        embed.addField("Joined on: ", String.valueOf(event.getAuthor().getCreationTime()), false);
-        event.getChannel().sendMessage(embed.build()).submit();
+        embed.addField("User ID: ", user.getAvatarId(), false);
+        embed.addField("Roles", String.valueOf(member.getRoles()), false);
+        embed.addField("Joined on: ", String.valueOf(user.getCreationTime()), false);
+        event.getChannel().sendMessage(embed.build()).queue();
     }
 }
