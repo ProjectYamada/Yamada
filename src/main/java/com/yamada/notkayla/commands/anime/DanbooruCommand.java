c5dfc5db8f4276fa4b6287414f07bb02f91be164
Albert Tang <tangalbert919@gmail.com>
2018-08-28 21:39:18 -0500

    Stick to normal checks while Sanae fixes Kayla checks

--- src/main/java/com/yamada/notkayla/commands/anime/DanbooruCommand.java
+++ src/main/java/com/yamada/notkayla/commands/anime/DanbooruCommand.java
@@ -1,7 +1,6 @@
 package com.yamada.notkayla.commands.anime;
 
 import com.yamada.notkayla.commands.Command;
-import com.yamada.notkayla.commands.Checks;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -18,7 +17,7 @@ import java.io.IOException;
 public class DanbooruCommand {
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
         String params = "[-status]=deleted";
-        if (Checks.isNotNSFW(event.getChannel())) {
+        if (!event.getChannel().isNSFW()) {
             event.getChannel().sendMessage("You need to be in a NSFW channel for this.").queue();
             return;
         }
