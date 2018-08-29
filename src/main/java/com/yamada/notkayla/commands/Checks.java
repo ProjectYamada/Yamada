2a081b90fc80662c25e092c1771f1198ffd21092
Sanae6 <atmatm6000@outlook.com>
2018-08-28 18:34:32 -0600

    e

--- src/main/java/com/yamada/notkayla/commands/Checks.java
+++ src/main/java/com/yamada/notkayla/commands/Checks.java
@@ -1,18 +1,22 @@
 package com.yamada.notkayla.commands;
 
 import com.yamada.notkayla.Config;
+import com.yamada.notkayla.Kayla;
 import net.dv8tion.jda.core.entities.TextChannel;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 import static com.yamada.notkayla.Config.*;
 
 public class Checks {
-    private static Object owners = null;
+    private static ArrayList<String> owners = null;
     public static boolean isNotAdmin(String id){
+        Kayla.log.log(Level.INFO, String.valueOf(configuration));
         if (owners == null)
             owners =
-                configuration.get("owners");
+                    (ArrayList<String>) configuration.get("owners");
         return !((List<String>)owners).contains(id);
     }
 
