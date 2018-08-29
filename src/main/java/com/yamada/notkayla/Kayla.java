66df67736bc8e92686c221aeedc169859dacbf9a
Sanae6 <atmatm6000@outlook.com>
2018-08-28 20:58:20 -0600

    java says this is an ArrayList and not a List so i'm rolling with it

--- src/main/java/com/yamada/notkayla/Kayla.java
+++ src/main/java/com/yamada/notkayla/Kayla.java
@@ -16,6 +16,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,10 +29,11 @@ public class Kayla {
     public static CommandRegistry registry = new CommandRegistry();
     public static Reflections refl = new Reflections();
     public static Map configuration;
-    public static List<String> owners;
+    private static List<String> owners;
     private static HashMap<String, Adapter> modules;
-    static Yaml yaml = new Yaml();
+    private static Yaml yaml = new Yaml();
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args){
         Kayla.log.log(Level.INFO,"Setting up config");
         try{
@@ -39,7 +41,7 @@ public class Kayla {
             Path config = Paths.get(curdir.toString(),"config.yml");
             configuration = (Map) yaml.load(new FileInputStream(config.toFile()));
             System.out.println(configuration.get("owners").getClass().getName());
-            owners = (List<String>) configuration.get("owners");
+            owners = (ArrayList<String>) configuration.get("owners");
         } catch (FileNotFoundException e) {
             Kayla.log.log(Level.SEVERE, "FileNotFoundError: file 'config.yml' does not exist");
             e.printStackTrace();
