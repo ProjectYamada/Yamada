949e2feea8fddb790c230fd4a9a7fef0e2f37df9
Sanae6 <atmatm6000@outlook.com>
2018-08-28 21:52:56 -0600

    ()

--- src/main/java/com/yamada/notkayla/commands/owner/ReloadCommand.java
+++ src/main/java/com/yamada/notkayla/commands/owner/ReloadCommand.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 @Command(name="reload",group = "owner",hidden = true)
 public class ReloadCommand {
     public void run (JDA bot, GuildMessageReceivedEvent event,String[] args) throws IllegalAccessException, InstantiationException {
-        if(!Kayla.getOwners.contains(event.getAuthor().getId())) return;
+        if(!Kayla.getOwners().contains(event.getAuthor().getId())) return;
         if (args[0] == null || args[1] == null) {
             event.getChannel().sendMessage("choose `command` or `module` and set the object to reload as the argument").queue();
         }
