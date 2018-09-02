42f33c817aa11b1ac914b90e786bc33356961442
tangalbert919 <tangalbert919@gmail.com>
2018-09-01 23:58:23 -0500

    Part 1 of this mute command. It's really bad, isn't it?

--- src/main/java/com/yamada/notkayla/commands/mod/MuteCommand.java
+++ src/main/java/com/yamada/notkayla/commands/mod/MuteCommand.java
@@ -5,18 +5,21 @@ import com.yamada.notkayla.utils.GettingFiles;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.IPermissionHolder;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.List;
 
 @Command(name="mute",group="mod",description="Mutes a user.")
 public class MuteCommand {
+    IPermissionHolder holder;
+    Role role;
+    List<Role> roles;
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
         if (!event.getMember().hasPermission(Permission.KICK_MEMBERS)) GettingFiles.didYouJustTryThat(event.getChannel(),event.getAuthor().getIdLong());
         try {
-            if (args.length == 2) {
-                //bot.getCategoryById(event.getMessage().getCategory().getId()).getManager().putPermissionOverride();
-                //event.getMessage().getMentionedMembers().get(0);
-
-            }
+            event.getGuild().getRoles().add(role.createCopy().setPermissions().setColor(0xff0000).complete());
+            //event.getGuild().getController().addRolesToMember(event.getMessage().getMentionedMembers().get(0), event.getGuild().getRoles().get(event.getGuild().getRoles().size() - 1)).queue();
         } catch (Exception e) {
             event.getChannel().sendMessage("I couldn't mute the specified user.").queue();
             return;
