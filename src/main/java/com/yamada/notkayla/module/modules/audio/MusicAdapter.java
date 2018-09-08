c8d52bb8fd29113b742ede3852992b0949d76fa5
Sanae6 <atmatm6000@outlook.com>
2018-09-07 21:55:21 -0600

    adapter thing for bill

---
+++ src/main/java/com/yamada/notkayla/module/modules/audio/MusicAdapter.java
@@ -0,0 +1,9 @@
+package com.yamada.notkayla.module.modules.audio;
+
+import com.yamada.notkayla.module.Adapter;
+
+public class MusicAdapter extends Adapter {
+    public MusicAdapter() throws InstantiationException, IllegalAccessException {
+        super("com.yamada.notkayla.module.modules.audio.MusicModule.java");
+    }
+}
