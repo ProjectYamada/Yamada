8f8ce0b56d444bda8a30da389ef7f56b89adf300
Sanae6 <atmatm6000@outlook.com>
2018-09-11 20:03:56 -0600

    fixing reload

--- src/main/java/com/yamada/notkayla/commands/owner/ReloadCommand.java
+++ src/main/java/com/yamada/notkayla/commands/owner/ReloadCommand.java
@@ -13,15 +13,15 @@ public class ReloadCommand {
     public void run (JDA bot, GuildMessageReceivedEvent event,String[] args) throws IllegalAccessException, InstantiationException {
         if(bot.getGuildById("481210197453438996").getMemberById(event.getAuthor().getId()) == null) return;
         Kayla.log.log(Level.INFO, Arrays.toString(args));
-        if (args[0] == null || args[1] == null) {
+        if (args[1] == null || args[2] == null) {
             event.getChannel().sendMessage("choose `command` or `module` and set the object to reload as the argument").queue();
         }
         switch(args[0]){
             case "command":
-                Kayla.registry.reload(args[1]);
+                Kayla.registry.reload(args[2]);
                 break;
             case "module":
-                String response = Kayla.reloadModule(args[1]);
+                String response = Kayla.reloadModule(args[2]);
                 break;
             default:
                 event.getChannel().sendMessage("choose `command` or `module` for the first argument and set the object to reload as the second").queue();
