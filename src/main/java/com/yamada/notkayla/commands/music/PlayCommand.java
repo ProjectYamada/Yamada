f34f97d74e8a7901ce98996198aa6f19f5eec3b0
Albert Tang <tangalbert919@gmail.com>
2018-09-14 23:12:20 -0500

    Search results are ready

--- src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
+++ src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
@@ -1,8 +1,8 @@
 package com.yamada.notkayla.commands.music;
 
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioTrack;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.BasicAudioPlaylist;
 import com.yamada.notkayla.Kayla;
 import com.yamada.notkayla.commands.Command;
@@ -12,25 +12,35 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 @Command(name="play", description = "Play something.", group = "music")
 public class PlayCommand {
-    private StringBuilder term;
     private YoutubeSearchProvider searchProvider = new YoutubeSearchProvider(new YoutubeAudioSourceManager());
-    private BasicAudioPlaylist playlist;
     private EmbedBuilder embed = new EmbedBuilder();
-    private YoutubeAudioTrack[] track;
+    private String display_track = "";
 
-    //TODO: Add search capabilities.
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
-        term = new StringBuilder();
+        AudioTrack[] track = new AudioTrack[5];
+
+        // !yplay Gee Girls Generation
+        StringBuilder term = new StringBuilder();
         for (int i = 1; i < args.length; i++) {
             term.append(args[i]).append("+");
         }
-        playlist = (BasicAudioPlaylist) searchProvider.loadSearchResult(term.toString().substring(0, term.length() - 1));
-        //track = playlist.getTracks().get(0).getInfo().title;
+
+        // Our search results return as a playlist. We will only get the first 5 in that list.
+        BasicAudioPlaylist playlist = (BasicAudioPlaylist) searchProvider.loadSearchResult(term.toString().substring(0, term.length() - 1));
+        for (int i = 0; i < 5; i++) {
+            track[i] = playlist.getTracks().get(i);
+            display_track += "**[" + (i+1) + ". " + track[i].getInfo().title + "](" + track[i].getInfo().uri + ")**\n";
+        }
         embed.setTitle(playlist.getName().replace("+", " "));
-        embed.addField("Results", String.valueOf(playlist.getTracks().size()), false);
+        embed.addField("Results: \n", display_track, false);
         embed.setFooter(String.format("Requested by %s", event.getAuthor().getName()), event.getAuthor().getAvatarUrl());
         event.getChannel().sendMessage(embed.build()).queue();
+
+        // Clear the used variables.
         embed.clear();
-        //Kayla.music.loadAndPlay(event.getChannel(), String.join("", args));
+        display_track = "";
+
+        // TODO: Add user input so we don't default to first thing on the search results.
+        Kayla.music.loadAndPlay(event.getChannel(), track[0].getInfo().uri);
     }
 }
