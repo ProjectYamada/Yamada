5361e5d7f286a21a38fa5bfed7bd5da0446d0b59
Albert Tang <tangalbert919@gmail.com>
2018-09-14 22:02:06 -0500

    Initial start to YouTube search functonality

--- src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
+++ src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
@@ -1,6 +1,8 @@
 package com.yamada.notkayla.commands.music;
 
 
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
 import com.yamada.notkayla.Kayla;
 import com.yamada.notkayla.commands.Command;
 import net.dv8tion.jda.core.JDA;
@@ -8,9 +10,16 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 @Command(name="play", description = "Play something.", group = "music")
 public class PlayCommand {
+    private StringBuilder term = new StringBuilder();
     //TODO: Add search capabilities.
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
         //MusicAdapter music = (MusicAdapter) Kayla.modules.get("music");
+        YoutubeSearchProvider searchProvider = new YoutubeSearchProvider(new YoutubeAudioSourceManager());
+        for (int i = 1; i < args.length; i++) {
+            System.out.println(args[i]);
+            term.append(args[i]).append("+");
+        }
+        searchProvider.loadSearchResult(term.toString());
         Kayla.music.loadAndPlay(event.getChannel(), String.join("", args));
     }
 }
