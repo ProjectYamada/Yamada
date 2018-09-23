3fff115174078bb5be790299692942cc044da6d9
Sanae6 <atmatm6000@outlook.com>
2018-09-22 22:38:07 -0600

    looks like i can't write code smartly enough

--- src/main/java/com/yamada/notkayla/utils/Timeout.java
+++ src/main/java/com/yamada/notkayla/utils/Timeout.java
@@ -3,7 +3,7 @@ package com.yamada.notkayla.utils;
 public class Timeout{
     private int delay;
     private Thread runnable;
-    private boolean canceled = true;
+    private boolean canceled = false;
     public Timeout(int delay, Runnable run){
         this.delay = delay;
         runnable = new Thread(run);
