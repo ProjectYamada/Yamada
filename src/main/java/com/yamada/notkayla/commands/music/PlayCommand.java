89ef7dd4037ec796ae94cedf81a3777165cc2f62
tangalbert919 <tangalbert919@gmail.com>
2018-10-25 18:29:09 -0500

    Do not run search if user is not in voice channel

--- src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
+++ src/main/java/com/yamada/notkayla/commands/music/PlayCommand.java
@@ -11,14 +11,15 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
-import java.util.concurrent.ExecutionException;
-import java.util.function.BiConsumer;
-
 @Command(name="play", description = "Play something.", group = "music")
 public class PlayCommand {
     private YoutubeSearchProvider searchProvider = new YoutubeSearchProvider(new YoutubeAudioSourceManager());
 
     public void run(JDA bot, GuildMessageReceivedEvent event, String[] args) {
+        if (!event.getMember().getVoiceState().inVoiceChannel()){
+            event.getChannel().sendMessage("You must be in "+(event.getGuild().getSelfMember().getVoiceState().inVoiceChannel()?"my":"a")+" voice chat to use my music commands.").queue();
+            return;
+        }
         EmbedBuilder embed = new EmbedBuilder();
         if (args.length == 1){
             event.getChannel().sendMessage("Not enough arguments provided").queue();
@@ -37,13 +38,15 @@ public class PlayCommand {
         int size = playlist.getTracks().size() > 5 ? 5 : playlist.getTracks().size();
         for (int i = 0; i < size; i++) {
             track[i] = playlist.getTracks().get(i);
-            embed.addField((i+1)+". **"+track[i].getInfo().title +" by "+ track[i].getInfo().author + "**","**["+track[i].getInfo().uri+"](" + track[i].getInfo().uri + ")**",false);
+            embed.addField((i+1)+". **"+track[i].getInfo().title + "** (" + track[i].getInfo().length + ")",
+                    "Uploaded by " + track[i].getInfo().author,false);
         }
         embed.setTitle(playlist.getName().replace("+", " "));
         embed.setDescription("Use !ysel <number> to select a song.");
         embed.setFooter(String.format("Requested by %s", event.getAuthor().getName()), event.getAuthor().getAvatarUrl());
         event.getChannel().sendMessage(embed.build()).submit();
-        SelectionManager.Selection selection = SelectionManager.requestSelection(event.getAuthor().getIdLong(), 1, 5,1);
+        SelectionManager.Selection selection = SelectionManager.requestSelection(event.getAuthor().getIdLong(),
+                1, 5,1);
         selection.get().whenComplete((integer, throwable) -> {
             if (throwable == null) Kayla.music.loadAndPlay(event, track[integer -1].getInfo().uri);
         });
