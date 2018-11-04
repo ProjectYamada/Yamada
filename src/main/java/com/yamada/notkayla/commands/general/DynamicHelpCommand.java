68a411f45c0c5dc0a84cedf7592a5121d9e74f50
Sanae <atmatm6000@outlook.com>
2018-11-03 22:38:44 -0600

    yeah i'll work on dynamic help right after music tomorrow or something

---
+++ src/main/java/com/yamada/notkayla/commands/general/DynamicHelpCommand.java
@@ -0,0 +1,12 @@
+package com.yamada.notkayla.commands.general;
+
+import com.yamada.notkayla.commands.Command;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+@Command(name="whelp",group="general")
+public class DynamicHelpCommand {
+    public void run(JDA bot, GuildMessageReceivedEvent event, String[] args){
+        event.getChannel().sendMessage("shut up im not done ok").queue();
+    }
+}
